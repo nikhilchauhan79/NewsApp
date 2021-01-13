341f51003079bad48a578d2e8bad6323414a4cbe
nikhilchauhan79 <nikhilchauhan79@gmail.com>
2021-01-13 21:29:16 +0530

    some todo's

--- app/src/main/java/com/example/newz/MainActivity.java
+++ app/src/main/java/com/example/newz/MainActivity.java
@@ -49,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
     //todo remove the extra lines and spaces from code base, make code clean
     private RecyclerView recyclerView;
     private Activity selectedActivity;
-
     private ArrayList<SourceParent> sourceParentArrayList;
     //todo implement bottom navigation with right way, refer some blogs
 
